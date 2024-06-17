dce1b1e6b648a0cd61b33c32c00a54dc83224eb9
Szymon Mentel <szymon@fly.io>
2024-06-17 12:09:49 +0200

    Add fly-replay header for mismatched versions

--- server.js
+++ server.js
@@ -4,8 +4,12 @@ const app = express();
 const version = process.env.VERSION;
 
 app.get(`/api/${version}/welcome/:name`, async (request, response) => {
-  console.log(`[${version}] Received request for ${request.params.name}`);
+  console.log(`[${version}] Handling request for ${request.params.name}`);
   response.send(`Hello ${request.params.name}! This is app version ${version}.`);
+});
+app.get(`/api/:version/welcome/:name`, (request, response) => {
+  console.log(`[${version}] Replaying request for ${request.params.name}`);
+  response.set('fly-replay', `app=replay-js-${request.params.version}`).send();
 });
 app.get('*', (_request, response) => {
   response.status(404).send("<div>404 Not Found</div>");
