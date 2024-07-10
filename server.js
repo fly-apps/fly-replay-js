1db77274a8fbb120747e03154f40c96a5be52391
Szymon Mentel <szymon@fly.io>
2024-07-10 13:48:46 +0200

    Simplify

--- server.js
+++ server.js
@@ -1,20 +1,19 @@
 import express from 'express';
-
 const app = express();
-const version = process.env.VERSION;
+const vsn = process.env.VERSION;
 
-app.get(`/api/${version}/welcome/:name`, async (request, response) => {
-  console.log(`[${version}] Handling request for ${request.params.name}`);
-  response.send(`Hello ${request.params.name}! This is app version ${version}.`);
+app.get(`/api/${vsn}/hi/:name`, (req, resp) => {
+  console.log(`[${vsn}] Serving ${req.params.name}`);
+  resp.send(`Hi ${req.params.name}! This is version ${vsn}.`);
 });
-app.get(`/api/:version/welcome/:name`, (request, response) => {
-  console.log(`[${version}] Replaying request for ${request.params.name}`);
-  response.set('fly-replay', `app=replay-js-${request.params.version}`).send();
+app.get(`/api/:vsn/hi/:name`, (req, resp) => {
+  console.log(`[${vsn}] Replaying for ${req.params.name}`);
+  resp.set('fly-replay', `app=fly-replay-${req.params.vsn}`).send();
 });
-app.get('*', (_request, response) => {
-  response.status(404).send("<div>404 Not Found</div>");
+app.get('*', (_req, resp) => {
+  resp.status(404).send("Not Found");
 });
 
 app.listen(3000, () => {
-  console.log(`Server with app version ${version} is listening on port 3000`);
+  console.log(`App version ${vsn} listens on port 3000`);
 });
